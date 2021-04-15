c8f7bacac68ccd7708965494f2d4dfbd11ccc59d
Andrew Ogilvy <ogilvyandrew@gmail.com>
2021-04-15 14:18:43 +0100

    get teams and team

--- teams/views.py
+++ teams/views.py
@@ -1,9 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response 
 from rest_framework import status
+from rest_framework.exceptions import NotFound
 
 from .models import Team
 from .serializers.common import TeamSerializer
+
 
 
 class TeamListView(APIView):
@@ -16,7 +18,15 @@ class TeamListView(APIView):
 
 class TeamDetailView(APIView):
 
+    def get_team(self, pk):
+        try:
+            return Team.objects.get(pk=pk)
+        except Team.DoesNotExist:
+            raise NotFound(detail="Cannot find that team")
+
     def get(self, _request, pk):
-        team = Team.objects.get(pk=pk)
+        team = self.get_team(pk=pk)
         serialized_team = TeamSerializer(team)
         return Response(serialized_team.data, status=status.HTTP_200_OK)
+
+
