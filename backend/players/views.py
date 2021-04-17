875461e009493115efbea4d7fbead11ed168864e
Andrew Ogilvy <ogilvyandrew@gmail.com>
2021-04-17 16:52:54 +0100

    finished seeds

--- backend/players/views.py
+++ backend/players/views.py
@@ -13,3 +13,9 @@ class PlayerListView(APIView):
             player_to_create.save()
             return Response(player_to_create.data, status=status.HTTP_201_CREATED)
         return Response(player_to_create.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+
+    def get(self, _request):
+        players = Player.objects.get()
+        serialized_players = PlayerSerializer(players, many=True)
+        return Response(serialized_players.data, status=status.HTTP_200_OK)
